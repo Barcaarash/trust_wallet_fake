1e6f36f1f7690c17c996ae2a0d3544db3a9ffe77
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-12-24 02:46:13 +0330

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,19 @@ export default function RootLayout({
 		<head>
 			<link rel='stylesheet' href='/copied.css'/>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+			<meta name="viewport"
+				 content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"/>
+			<meta name="apple-mobile-web-app-capable" content="BingX, by iTzUnity"/>
+			<meta name="format-detection" content="telephone=yes"/>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+			<meta name="apple-mobile-web-app-capable" content="BingX H5"/>
+			<meta name="apple-mobile-web-app-status-bar-style" content="#0a0b0d"/>
+			<meta name="theme-color" content="#0a0b0d"/>
+			<meta name="description"
+				 content="Crypto Trading Made Easy. Buy and sell with BingX, a secure platform that makes it easy to trade and store cryptocurrency."/>
+			<meta name="keywords" content="Ethereum Perpetual Futures, Safe Trading, Digital Assets"/>
+			<meta name="apple-mobile-web-app-capable" content="yes"/>
 		</head>
 		<body>
 		{children}
